22ad741c474043ce4d6e89b7b0f3a8ba03a7358c
GuiQRSM <quaresmagui20@gmail.com>
2025-09-11 09:59:58 -0300

    frat: declare atget.tsx at app folder

---
+++ src/app/target.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Target() {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Target</Text>
+        </View>
+    )
+}
