ee9bc84fd0777d98cf1ca183e8060e637fea8278
GuiQRSM <quaresmagui20@gmail.com>
2025-09-12 10:23:48 -0300

    example: defines the font for test

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,13 @@
 import { View, Text, Button } from "react-native";
 import { router } from "expo-router";
+import { fontFamily } from "@/theme/fontFamily";
 
 export default function Index() {
 
     
     return (
         <View style= {{flex: 1, justifyContent: 'center', alignItems: 'center',}}>
-            <Text style={{padding: 16}}>Olá, Expo Router</Text>
+            <Text style={{padding: 16, fontFamily: fontFamily.bold }}>Olá, Expo Router</Text>
 
             <Button  title="navegar" onPress={() => router.navigate("/target")} />
             <View style={{padding: 4}}/>
