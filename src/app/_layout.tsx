a7cda1bb1ec997d67b41fb01f197db108efa2639
GuiQRSM <quaresmagui20@gmail.com>
2025-09-12 12:41:17 -0300

    reafctor: turns fontIsLoaded as !fontIsLoaded

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,20 +1,30 @@
-import { Stack } from "expo-router";
-import { colors } from "@/theme/colors";
-import {useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold} from "@expo-google-fonts/poppins"
-import { Loading } from "@/components/Loading";
-
+import { Stack } from 'expo-router';
+import { colors } from '@/theme/colors';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+import { Loading } from '@/components/Loading';
 
 export default function Layout() {
+  const [fontIsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
 
-    const [fontIsLoaded] = useFonts({
-         Poppins_400Regular, Poppins_500Medium, Poppins_700Bold
-    })
+  if (!fontIsLoaded) {
+    return <Loading />;
+  }
 
-    if(fontIsLoaded) {
-        return <Loading />
-    }
-
-    return (
-        <Stack screenOptions={{headerShown: false, contentStyle: {backgroundColor: colors.white}}} />
-    )
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: colors.white },
+      }}
+    />
+  );
 }
