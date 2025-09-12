8dbd41be06d6fa513404011e96821ef2be5bb8c7
GuiQRSM <quaresmagui20@gmail.com>
2025-09-12 09:50:19 -0300

    feat: defines the loading of fonts in the layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,18 @@
 import { Stack } from "expo-router";
 import { colors } from "@/theme/colors";
+import {useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold} from "@expo-google-fonts/poppins"
+
 
 export default function Layout() {
-    console.log("Passou pelo layout primeiro")
+
+    const [fontIsLoaded] = useFonts({
+         Poppins_400Regular, Poppins_500Medium, Poppins_700Bold
+    })
+
+    if(!fontIsLoaded) {
+        return
+    }
+
     return (
         <Stack screenOptions={{headerShown: false, contentStyle: {backgroundColor: colors.white}}} />
     )
