3dcacc4dd528c02edec098da535c616810bf2c0c
GuiQRSM <quaresmagui20@gmail.com>
2025-09-12 11:31:26 -0300

    refactor: clear the index app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,5 @@
-import { View, Text, Button } from "react-native";
-import { router } from "expo-router";
-import { fontFamily } from "@/theme/fontFamily";
+import { View } from 'react-native';
 
 export default function Index() {
-
-    
-    return (
-        <View style= {{flex: 1, justifyContent: 'center', alignItems: 'center',}}>
-            <Text style={{padding: 16, fontFamily: fontFamily.bold }}>Olá, Expo Router</Text>
-
-            <Button  title="navegar" onPress={() => router.navigate("/target")} />
-            <View style={{padding: 4}}/>
-           <Button title="Transação" onPress={() => router.navigate("/transaction/8")}/>
-            <View style={{padding: 4}}/>
-            <Button title="Progresso" onPress={() => router.navigate("/in-progress/12")}/>
-        </View>
-    )
+  return <View style={{ flex: 1 }}></View>;
 }
