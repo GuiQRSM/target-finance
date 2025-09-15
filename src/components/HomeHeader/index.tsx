897ccc35a3e224b89759841064545e4877151354
GuiQRSM <quaresmagui20@gmail.com>
2025-09-15 09:24:51 -0300

    feat: add a label at the header

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,3 +1,4 @@
+import { View, Text } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { colors } from '@/theme/colors';
 import { styles } from './styles';
@@ -7,6 +8,10 @@ export function HomeHeader() {
     <LinearGradient
       colors={[colors.blue[500], colors.blue[800]]}
       style={styles.container}
-    />
+    >
+      <View>
+        <Text style={styles.label}>Total que você possui</Text>
+      </View>
+    </LinearGradient>
   );
 }
