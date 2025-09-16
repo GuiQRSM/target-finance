c0223e07952a7ea86a914d7f18daaedbbaa5128a
GuiQRSM <quaresmagui20@gmail.com>
2025-09-16 09:58:23 -0300

    feat: new property addition in Summary Props typing

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ColorValue } from 'react-native';
+import { View, ColorValue, Text } from 'react-native';
 import { styles } from './styles';
 import { MaterialIcons } from '@expo/vector-icons';
 
@@ -7,19 +7,24 @@ export type SummaryProps = {
   value: string;
 };
 
-type Props = {
+export type Props = {
   data: SummaryProps;
   icon: {
     name: keyof typeof MaterialIcons.glyphMap;
     color: ColorValue;
   };
+  isLeft?: boolean;
 };
 
-export function Summary({ data, icon }: Props) {
+export function Summary({ data, icon, isLeft = false }: Props) {
   return (
-    <View style={styles.header}>
-      <MaterialIcons name={icon.name} size={16} color={icon.color} />
-      <Text style={styles.label}>{data.label}</Text>
+    <View style={styles.container}>
+      <View style={[styles.header, isLeft && { justifyContent: 'flex-end' }]}>
+        <MaterialIcons name={icon.name} size={16} color={icon.color} />
+        <Text style={styles.label}>{data.label}</Text>
+      </View>
+
+      <Text style={styles.value}>{data.value}</Text>
     </View>
   );
 }
