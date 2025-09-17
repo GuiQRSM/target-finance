d1aae1f1ce9f39393f3d6dae1362147eaba48d4b
GuiQRSM <quaresmagui20@gmail.com>
2025-09-17 09:19:13 -0300

    add: some import and props for the type

--- src/components/Target/index.tsx
+++ src/components/Target/index.tsx
@@ -1,12 +1,24 @@
-import {} from 'react-native';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from 'react-native';
 import { styles } from './style';
+import { MaterialIcons } from '@expo/vector-icons';
 
-export type TargetProps = {};
+export type TargetProps = TouchableOpacityProps & {
+  id?: string;
+  name: string;
+  percentage: string;
+  current: string;
+  target: string;
+};
 
 type Props = {
   data: TargetProps;
 };
 
-export function Target() {
+export function Target({ data }: Props) {
   return;
 }
