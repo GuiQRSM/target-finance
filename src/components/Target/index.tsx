e26f4dd605279a3ab31d28b83157e07cc532d09d
GuiQRSM <quaresmagui20@gmail.com>
2025-09-17 09:09:36 -0300

    faet: Target component

---
+++ src/components/Target/index.tsx
@@ -0,0 +1,12 @@
+import {} from 'react-native';
+import { styles } from './style';
+
+export type TargetProps = {};
+
+type Props = {
+  data: TargetProps;
+};
+
+export function Target() {
+  return;
+}
