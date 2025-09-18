35d60e1be9897cddb09477c074a0010660278e2c
GuiQRSM <quaresmagui20@gmail.com>
2025-09-18 11:28:13 -0300

    add: call Button componete at the index od the app folder

--- src/components/Butoon/index.tsx
+++ src/components/Butoon/index.tsx
@@ -12,4 +12,21 @@ type Props = TouchableOpacityProps & {
   isProcessing?: boolean;
 };
 
-export function Button() {}
+export function Button({ title, isProcessing = false, ...rest }) {
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      activeOpacity={0.8}
+      disabled={false}
+      {...rest}
+    >
+      <Text style={styles.title}>
+        {isProcessing ? (
+          <ActivityIndicator size="small" color={colors.white} />
+        ) : (
+          title
+        )}
+      </Text>
+    </TouchableOpacity>
+  );
+}
