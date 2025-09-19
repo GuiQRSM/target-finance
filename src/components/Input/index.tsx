d4fcc42226038fc48f65ed213baac048f0a3e067
GuiQRSM <quaresmagui20@gmail.com>
2025-09-19 10:57:36 -0300

    add: function and typing of the Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,21 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, TextInputProps } from 'react-native';
 import { colors } from '@/theme';
 import { styles } from './style';
 
-type Props = {};
+type Props = TextInputProps & {
+  label: string;
+};
 
-export function Input({}) {}
+export function Input({ label, ...rest }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholderTextColor={colors.gray[400]}
+        {...rest}
+      />
+    </View>
+  );
+}
