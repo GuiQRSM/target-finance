0aeea882501f29142102c4b72fcc6bf787209684
GuiQRSM <quaresmagui20@gmail.com>
2025-09-19 09:52:34 -0300

    add: conditional handling for rendering the right icon/button

--- src/components/pageHeader/index.tsx
+++ src/components/pageHeader/index.tsx
@@ -7,21 +7,29 @@ import { router } from 'expo-router';
 type Props = {
   title: string;
   subtitle?: string;
-  rightButton?: {
+  rightButtom?: {
     onPress: () => void;
     icon: keyof typeof MaterialIcons.glyphMap;
   };
 };
 
-export function PageHeader({ title, subtitle, rightButton }: Props) {
+export function PageHeader({ title, subtitle, rightButtom }: Props) {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity activeOpacity={0.8} onPress={() => router.back()}>
+        <TouchableOpacity activeOpacity={0.8} onPress={() => router.back}>
           <MaterialIcons name="arrow-back" size={32} color={colors.black} />
         </TouchableOpacity>
+        {rightButtom && (
+          <TouchableOpacity onPress={rightButtom.onPress}>
+            <MaterialIcons
+              name={rightButtom.icon}
+              size={24}
+              color={colors.gray[500]}
+            />
+          </TouchableOpacity>
+        )}
       </View>
-
       <Text style={styles.title}>{title}</Text>
       {subtitle && <Text style={styles.subtitle}>{subtitle}</Text>}
     </View>
