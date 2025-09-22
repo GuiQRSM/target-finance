9554acbaba43f6a5659fe23a24327e1eb0899349
GuiQRSM <quaresmagui20@gmail.com>
2025-09-22 10:11:49 -0300

    add: call PageHeader in 'InProgress'

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,14 +1,19 @@
-import { View, Text, Button } from "react-native"
-import { router, useLocalSearchParams } from "expo-router"
+import { View } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+import { PageHeader } from '@/components/pageHeader';
 
 export default function InProgress() {
-const params = useLocalSearchParams<{id: string}>()
+  const params = useLocalSearchParams<{ id: string }>();
 
-    return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>InProgress: {params.id}</Text>
-
-            <Button title="voltar" onPress={() => router.back()} />
-        </View>
-    )
+  return (
+    <View style={{ flex: 1, padding: 24, gap: 32 }}>
+      <PageHeader
+        title="Apple Watch"
+        rightButtom={{
+          icon: 'edit',
+          onPress: () => {},
+        }}
+      />
+    </View>
+  );
 }
