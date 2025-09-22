fc705db264920cfa7d8006ca3dae6e727f591b95
GuiQRSM <quaresmagui20@gmail.com>
2025-09-22 10:43:49 -0300

    add: Progress component index.tsx settings

--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -1,9 +1,34 @@
-import { View } from 'react-native';
-import { colors } from '@/theme';
+import { View, Text } from 'react-native';
 import { styles } from './styles';
 
-type Props = {
-  label: string;
+type SavedValues = {
+  current: string;
+  target: string;
+  percentage: number;
 };
 
-export function Progress() {}
+type Props = {
+  data: SavedValues;
+};
+
+export function Progress({ data }: Props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Valor guardado</Text>
+
+      <View style={styles.status}>
+        <Text style={styles.value}>
+          {data.current}
+
+          <Text style={styles.target}> de {data.target}</Text>
+        </Text>
+      </View>
+
+      <View style={styles.progress}>
+        <View
+          style={[styles.currentProgress, { width: `${data.percentage}%` }]}
+        />
+      </View>
+    </View>
+  );
+}
