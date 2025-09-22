a6e94a11ccdc88c41bb4039348adc17afa3d80f6
GuiQRSM <quaresmagui20@gmail.com>
2025-09-22 10:16:46 -0300

    feat: add Progress component

---
+++ src/components/Progress/index.tsx
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+import { colors } from '@/theme';
+import { styles } from './styles';
+
+type Props = {
+  label: string;
+};
+
+export function Progress() {}
---
+++ src/components/Progress/styles.ts
@@ -0,0 +1,6 @@
+import { StyleSheet } from 'react-native';
+import { colors, fontFamily } from '@/theme';
+
+export const styles = StyleSheet.create({
+  container: {},
+});
