94e823a7659092c24e5649240e4a3ef7aa5a35b6
GuiQRSM <quaresmagui20@gmail.com>
2025-09-23 10:15:44 -0300

    add: defines the typing props and calls the enum as a prop typing

--- src/components/Transaction/index.tsx
+++ src/components/Transaction/index.tsx
@@ -2,8 +2,15 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { colors } from '@/theme';
 import { styles } from './styles';
+import { TransactionType } from '@/utils/TransactionTypes';
 
-type TransactionProps = {};
+type TransactionProps = {
+  id: string;
+  value: string;
+  date: string;
+  description?: string;
+  type: TransactionType;
+};
 
 export function Transaction() {
   return;
