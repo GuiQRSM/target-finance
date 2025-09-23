2ba7574cf05f8220d439be771552840e697ac7e3
GuiQRSM <quaresmagui20@gmail.com>
2025-09-23 10:04:31 -0300

    feat: adds a new component, the Transaction

---
+++ src/components/Transaction/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+import { colors } from '@/theme';
+import { styles } from './styles';
+
+type TransactionProps = {};
+
+export function Transaction() {
+  return;
+}
