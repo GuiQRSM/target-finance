28bb942f8858d0c815599e14a4125ea6ac3bf988
GuiQRSM <quaresmagui20@gmail.com>
2025-09-24 11:01:34 -0300

    add: call PageHeader component in the [id].tsx from 'transaction' folder

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,15 +1,19 @@
-import { View, Text, Button } from "react-native"
-import { useLocalSearchParams, router } from "expo-router"
+import { View } from 'react-native';
+import { useLocalSearchParams, router } from 'expo-router';
+import { PageHeader } from '@/components/pageHeader';
+import { InputCurrency } from '@/components/InputCurrency';
+import { Input } from '@/components/Input';
+import { Button } from '@/components/Butoon';
 
 export default function Transaction() {
+  const params = useLocalSearchParams<{ id: string }>();
 
-    const params = useLocalSearchParams<{id: string}>()
-
-    return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={{fontSize: 26, fontWeight: 'bold', color: "black"}}>ID: {params.id}</Text>
-
-            <Button title="voltar" onPress={() => router.back()}/>
-        </View>
-    )
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      <PageHeader
+        title="Nova transação"
+        subtitle="A cada valor guardado você fica mais próximo da sua meta. Se esforce para guardar e evitar retirar"
+      />
+    </View>
+  );
 }
