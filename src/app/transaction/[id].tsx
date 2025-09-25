b2890ff409920a356b5af24db6e24356cbfa2f40
GuiQRSM <quaresmagui20@gmail.com>
2025-09-25 10:12:43 -0300

    add: calls TransactionTypeSwitch in the '[id].tsx'

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,11 +1,15 @@
 import { View } from 'react-native';
+import { useState } from 'react';
+import { TransactionType } from '@/utils/TransactionTypes';
 import { useLocalSearchParams } from 'expo-router';
 import { PageHeader } from '@/components/pageHeader';
 import { InputCurrency } from '@/components/InputCurrency';
 import { Input } from '@/components/Input';
 import { Button } from '@/components/Butoon';
+import { TransactionTypeSwitch } from '@/components/TransactionType';
 
 export default function Transaction() {
+  const [type, setType] = useState(TransactionType.Input);
   const params = useLocalSearchParams<{ id: string }>();
 
   return (
@@ -16,6 +20,7 @@ export default function Transaction() {
       />
 
       <View style={{ marginTop: 32, gap: 24 }}>
+        <TransactionTypeSwitch selected={type} onChange={setType} />
         <InputCurrency label="Valor (R$)" value={0} />
         <Input
           label="Motivo (opcional)"
