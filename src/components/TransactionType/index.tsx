66a6b2b8c4e19e1484680c86cdf68bdf09f86f75
GuiQRSM <quaresmagui20@gmail.com>
2025-09-25 10:00:15 -0300

    add: defines the structure of the 'index.tsx' of the TransactionTypes component

--- src/components/TransactionType/index.tsx
+++ src/components/TransactionType/index.tsx
@@ -1,7 +1,29 @@
 import { View } from 'react-native';
 import { colors } from '@/theme';
 import { styles } from './style';
+import { Option } from './option';
+import { TransactionType } from '@/utils/TransactionTypes';
 
-type TrasanctionTypeProps = {};
+type Props = {
+  selected: TransactionType;
+  onChange: (type: TransactionType) => void;
+};
 
-export function TransactionType() {}
+export function TransactionTypes({ selected, onChange }: Props) {
+  return (
+    <View style={styles.container}>
+      <Option
+        icon="arrow-upward"
+        title="Guardar"
+        isSelected={selected === TransactionType.Input}
+        selectedColor={colors.blue[500]}
+      />
+      <Option
+        icon="arrow-downward"
+        title="Resgatar"
+        isSelected={selected === TransactionType.Output}
+        selectedColor={colors.red[400]}
+      />
+    </View>
+  );
+}
