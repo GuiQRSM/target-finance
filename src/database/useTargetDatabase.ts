cea2f552978b9bb4fe0eba6969b361821642a2c7
GuiQRSM <quaresmagui20@gmail.com>
2025-09-30 10:24:00 -0300

    add?: Implement a const database receiving SQLiteDatabase

--- src/database/useTargetDatabase.ts
+++ src/database/useTargetDatabase.ts
@@ -1,9 +1,13 @@
+import { SQLiteDatabase } from 'expo-sqlite';
+
 export type TargetProps = {
   name: string;
   amount: number;
 };
 
 export function useTargetsDatabase() {
+  const database = SQLiteDatabase;
+
   async function create(data: TargetProps) {}
 
   return {
