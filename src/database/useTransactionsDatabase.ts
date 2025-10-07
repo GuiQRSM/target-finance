ded15ca718ca72fbf31034ab02e5e1403f3d80ee
GuiQRSM <quaresmagui20@gmail.com>
2025-10-07 09:35:50 -0300

    add: creates the create method and implements the TransactionCreate type

--- src/database/useTransactionsDatabase.ts
+++ src/database/useTransactionsDatabase.ts
@@ -1,5 +1,17 @@
 import { useSQLiteContext } from 'expo-sqlite';
 
+export type TransactionCreate = {
+  target_id: number;
+  amount: number;
+  observation?: string;
+};
+
 export function useTransactionsDatabase() {
   const database = useSQLiteContext();
+
+  async function create(data: TransactionCreate) {}
+
+  return {
+    create,
+  };
 }
