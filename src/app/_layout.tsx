a1057310ce6de49b68fe982a8ca552275cad2f27
GuiQRSM <quaresmagui20@gmail.com>
2025-10-13 12:56:52 -0300

    refactor: reorganizes imports

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,16 @@
-import { Suspense } from 'react';
-import { Stack } from 'expo-router';
+import { Loading } from '@/components/Loading';
 import { colors } from '@/theme/colors';
+
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
-import { Loading } from '@/components/Loading';
+import { Suspense } from 'react';
+import { Stack } from 'expo-router';
 import { SQLiteProvider } from 'expo-sqlite';
+
 import { migrate } from '@/database/migrate';
 
 export default function Layout() {
